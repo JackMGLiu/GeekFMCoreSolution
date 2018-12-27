fc9671ac5fe5ed2716bd02a7859f441f99c64007
Jack-Liu <love_zs1314@126.com>
2018-12-27 08:59:31 +0800

    log

--- Geek.Project.Infrastructure/DataBase/ProjectDbContext.cs
+++ Geek.Project.Infrastructure/DataBase/ProjectDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Geek.Project.Entity;
 using Geek.Project.Entity.Configurations;
+using Geek.Project.Entity.EntityConfigurations;
 using Microsoft.EntityFrameworkCore;
 
 namespace Geek.Project.Infrastructure.DataBase
@@ -13,13 +14,15 @@ namespace Geek.Project.Infrastructure.DataBase
 
         public DbSet<SysUser> SysUsers { get; set; }
         public DbSet<SysRole> SysRoles { get; set; }
-        public DbSet<SysLog> sysLogs { get; set; }
+        public DbSet<SysLog> SysLogs { get; set; }
+        public DbSet<BlogArticle> BlogArticles { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new SysUserConfiguration());
             modelBuilder.ApplyConfiguration(new SysRoleConfiguration());
             modelBuilder.ApplyConfiguration(new SysLogConfiguration());
+            modelBuilder.ApplyConfiguration(new BlogArticleConfiguration());
             base.OnModelCreating(modelBuilder);
         }
     }
